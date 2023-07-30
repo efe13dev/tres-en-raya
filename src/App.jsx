c76c626a229ef30cab5500da9cc9077892c531b0
efe <eefee1987@gmail.com>
2023-07-30 12:52:33 +0200

    add logic board

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import './App.css';
-import Board from './components/Board'
+import Board from './components/Board';
 
 function App() {
-  
   return (
     <>
+      <h1>Tres en raya</h1>
       <Board />
     </>
   );
